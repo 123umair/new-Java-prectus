a615b5e819c0734a0d0323f1957b9aaca07df179
Muhammad Umair khan <141265429+123umair@users.noreply.github.com>
2024-06-17 12:30:03 +0000

    2nd file added

---
+++ 01_basic/02_basic.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("uk") So we are using vs code not browser
+// console.log(3
+//     + 
+//     3) code readability should be high so we try to write in good way like console.log(3+3)
+
+ console.log("Hitesh") 
+
+//  Primitive Data type
+//  number(range) => 2^53 mostly we use number 
+//  bigint = We use this data type if the number is too high mostly used in trading etc
+//  String => "" best to used in double quotes
+// boolean => true/false
+// null => Null is basically object data type, standalone value it is the representation of empty value
+// undefined => means value on the spot  not assigned
+//   symbol => unique basically used for uniqueness
+let Name = "Umair khan"
+let age = 18 
+let isLoggedIn = false
+let Status 
+let Status_two = null
+
+console.log(typeof Name)    
+console.log(typeof age)   
+console.log(typeof isLoggedIn)    
+// console.log(typeof Status)
+console.log(typeof null)
+// console.log(typeof Status_two) 
+console.log(typeof undefined)
+
+
+console.table([(typeof Name)])
