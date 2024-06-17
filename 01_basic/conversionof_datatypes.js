8f365363abc3fafe81f72ece57a045bd16a40b36
Muhammad Umair khan <141265429+123umair@users.noreply.github.com>
2024-06-17 12:49:23 +0000

    My 3rd file

---
+++ 01_basic/conversionof_datatypes.js
@@ -0,0 +1,54 @@
+let score = "33"
+// console.log(typeof (score))
+/* let ValueinNumber = Number(score)('Number is a data type it means that '
+that the score is now converted to the number data type either the score variable have sting value)
+] conversion of 
+data type method it is method type to check the data type
+by these method we use string also another data types*/
+let ValueinNumber = Number(score)
+console.log(score)
+console.log(typeof score)
+console.log(typeof(ValueinNumber))
+
+//for Sting we check this 
+let string = "umair"//we take sting is just a variable name
+ console.log(string)//output=umair
+console.log(typeof string)//it will string data type output will be string
+let conversion_of_datatype = Number(string)//String Data type convert to Number
+console.log(typeof conversion_of_datatype) //output=numbe but if we pring the value then it will be Nan because we canot its contain character that it will be sting datatype and can't change 
+//now we check its value
+console.log(conversion_of_datatype)//its output will NaN its mean that Not a number
+//lets we check the mixed number like stringvalue and number 
+let marks = "334dfe"
+console.log(typeof(marks)) //out will be string 
+let convert = Number(marks)//converted the string data type to Number data type
+console.log(convert)//output=Nan its canot be converted  
+console.log(typeof convert) //number because we change it 
+
+
+//we try for boolean values
+let isLogedIn = false //assigned false value
+console.log(isLogedIn) //output false
+console.log(typeof isLogedIn)// ouput=boolean
+console.log(isLogedIn) //false
+let change = Number(isLogedIn) //we change the data type
+console.log(change) //output=0 give the numeric value of the false
+console.log(typeof change)//output=number
+
+
+//lets now try to use boolean data type
+let Login = 1
+let bool=Boolean(Login)
+console.log(typeof bool) 
+console.log(bool) 
+// when we give different
+//1 => true; 0=>false
+//" "=>false
+//"hitesh" => true
+
+//through conversion with string
+let num = 44
+let stringinnumber = String(num)
+console.log(stringinnumber)
+console.log(typeof stringinnumber)
+
